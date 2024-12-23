2c5e02f34b400ec479fa60fb9761645d8ddbde40
AydenC77 <64756450+AydenC77@users.noreply.github.com>
2024-12-23 15:21:36 -0800

    Added /stopserver command to replace /stop so that server dosnt spit out errors when disabling plugin
    Added SkypixleServer.java:35-38 to prevent other plugins from disableing the plugin

--- src/main/java/net/skypixle/skypixleServer/SkypixleServer.java
+++ src/main/java/net/skypixle/skypixleServer/SkypixleServer.java
@@ -1,11 +1,8 @@
 package net.skypixle.skypixleServer;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -13,11 +10,13 @@ import java.util.concurrent.TimeUnit;
 public final class SkypixleServer extends JavaPlugin {
 
     private long startTime;
+    private boolean isServerShuttingDown;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         startTime = System.currentTimeMillis();
+        isServerShuttingDown = false;
 
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(() -> {
@@ -33,6 +32,18 @@ public final class SkypixleServer extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        //getServer().getPluginManager().enablePlugin(this);
+        if (isServerShuttingDown == false) {
+            getServer().getPluginManager().enablePlugin(this);
+            throw new UnsupportedOperationException("This plugin cannot be disabled!");
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equals("stopserver")) {
+            isServerShuttingDown = true;
+            getServer().shutdown();
+        }
+        return super.onCommand(sender, command, label, args);
     }
 }
